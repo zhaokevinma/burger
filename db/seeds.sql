49cf1949a39de87f93b597fede4d25e851f6d7b7
Kevin Malone <zhaokevinma@gmail.com>
2019-10-09 18:48:24 -0400

    initial commit - prior to any testing activities

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Crispy Buttermilk Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Beef double cheese');
+INSERT INTO burgers (burger_name) VALUES ('Fillet Fish');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sausage', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
