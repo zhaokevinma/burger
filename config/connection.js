77451f484cc78c52af1c208d2c78f190b7a613f4
Kevin Malone <zhaokevinma@gmail.com>
2019-10-10 17:59:18 -0400

    database connection issue fix

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var mysql = require("mysql");
 
 // connection object
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env, JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
